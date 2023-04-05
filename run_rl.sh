5871dcf10603a5e23169a5fae8528b5e5c0fd00c
Matt Edminster <matt@renegadelabs.io>
2023-04-05 13:49:21 -0400

    RTK auto select linux vs mac

--- run_rl.sh
+++ run_rl.sh
@@ -1,2 +1,13 @@
-#! /bin/bash
-poetry shell && poetry lock --no-update && poetry install && skybrushd -c ../skybridge-ext-rl-gaming/rl-skybrush.jsonc 
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OS
+  echo "Running on Mac OS"
+  CONFIG_FILE=rl-skybrush-mac.jsonc
+else
+  # Linux
+  echo "Running on Linux"
+  CONFIG_FILE=rl-skybrush-ubuntu.jsonc
+fi
+
+poetry shell && poetry lock --no-update && poetry install && skybrushd -c ../skybridge-ext-rl-gaming/$CONFIG_FILE
