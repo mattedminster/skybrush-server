6c21150b0ccfc794b9294af5e1fd8ad633fc283c
Matt Edminster <matt@renegadelabs.io>
2023-05-12 14:00:56 -0400

    yeah yeah

--- run_rl.sh
+++ run_rl.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Function to handle script termination
+cleanup() {
+    echo "Terminating skybrushd..."
+    pkill -f "skybrushd"
+    exit
+}
+
+# Register cleanup function to execute on script termination
+trap cleanup EXIT
+
+# Determine configuration file based on OS
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # Mac OS
   echo "Running on Mac OS"
@@ -10,4 +21,5 @@ else
   CONFIG_FILE=rl-skybrush-ubuntu.jsonc
 fi
 
+# Activate poetry environment and run skybrushd
 source "$(~/.local/bin/poetry env info --path)/bin/activate" && ~/.local/bin/poetry lock --no-update && ~/.local/bin/poetry install && skybrushd -c ../skybridge-ext-rl-gaming/$CONFIG_FILE
