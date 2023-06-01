566ebcd39f34439c7432a15eb098223330d89243
Matt Edminster <matt@renegadelabs.io>
2023-05-31 20:21:33 -0400

    boo

--- src/flockwave/server/ext/mavlink/driver.py
+++ src/flockwave/server/ext/mavlink/driver.py
@@ -1291,7 +1291,7 @@ class MAVLinkUAV(UAVBase):
         )
         try:
             await self.driver.send_packet_with_retries(
-                message, self, wait_for_response=response, timeout=0.01, retries=0
+                message, self, wait_for_response=response, timeout=0.05, retries=1
             )
         except TooSlowError:
             # Maybe it's okay anyway, see comment above
