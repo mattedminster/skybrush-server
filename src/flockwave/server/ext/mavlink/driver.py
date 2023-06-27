e805e5ed2f0b69884a00b0d7c3de3c3886bc0455
Matt Edminster <matt@renegadelabs.io>
2023-06-27 16:59:35 -0400

    s

--- src/flockwave/server/ext/mavlink/driver.py
+++ src/flockwave/server/ext/mavlink/driver.py
@@ -1134,8 +1134,10 @@ class MAVLinkUAV(UAVBase):
         self, configuration: GeofenceConfigurationRequest
     ) -> None:
         """Configures the geofence on the UAV."""
-        self.driver.log.warn("Configuring geofence on UAV")
+        self.driver.log.warn("Configuring geofence on UAV: %r", configuration)
+        self.driver.log.warn("self.driver.geofence before: %r", self.driver.geofence)
         self.driver.geofence = configuration
+        self.driver.log.warn("self.driver.geofence after: %r", self.driver.geofence)
         return await self._autopilot.configure_geofence(self, configuration)
 
     async def configure_safety(self, configuration: SafetyConfigurationRequest) -> None:
