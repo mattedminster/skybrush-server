30d43f3fc936ba6515d36f34387519c6e1f07de3
Matt Edminster <matt@renegadelabs.io>
2023-08-05 20:09:56 -0400

    1.0.2408

--- src/flockwave/server/ext/mavlink/driver.py
+++ src/flockwave/server/ext/mavlink/driver.py
@@ -798,8 +798,6 @@ class MAVLinkDriver(UAVDriver):
         self, signals: List[str], duration: int, *, transport=None
     ) -> None:
         channel = transport_options_to_channel(transport)
-        self.driver.app.log.warn("Sending light or sound emission signal")
-        self.driver.app.log.warn("Signals: %s", signals)
         if "light" in signals:
 
             message = create_led_control_packet(broadcast=True)
@@ -809,8 +807,6 @@ class MAVLinkDriver(UAVDriver):
         self, uav: "MAVLinkUAV", signals: List[str], duration: int, *, transport=None
     ) -> None:
         channel = transport_options_to_channel(transport)
-        self.driver.app.log.warn("Sending light or sound emission signal")
-        self.driver.app.log.warn("Signals: %s", signals)
         if "light" in signals:
             message = create_led_control_packet()
             await self.send_packet(message, uav, channel=channel)
